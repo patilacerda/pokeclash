cc67c92f53901780a894421364994dce4683a332
patilacerda <>
2023-09-27 14:23:27 +0000

    Add function to players choice and random choice to opponent

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,11 +1,23 @@
+let player;
+let opponent;
 var cards = ["assets/images/bulbasaur.png", "assets/images/charmander.png", "assets/images/squirtle.png"];
 
 window.onload = function () {
     for (let i = 0; i < 3; i++) {
-        // <img id="rock" src="rock.png">
+     
         let card = document.createElement("img");
         card.id = cards[i];
         card.src = cards[i];
+        card.addEventListener("click", selectCard);
         document.getElementById("cards").append(card);
     }
 };
+
+function selectCard() {
+    player = this.id;
+    document.getElementById("player-choice").src = player;
+
+    opponent = cards[Math.floor(Math.random() * 3)];
+    document.getElementById("opponent-choice").src = opponent;
+
+}
