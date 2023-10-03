62ecd81c412d7daae8ef972e06c61522829aab34
patilacerda <>
2023-10-03 21:02:59 +0000

    Tie issue solved and add array for img alt text

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -12,6 +12,7 @@ let pokeball = "assets/images/pokeball.png";
 let gameEnded = false;
 
 let cards = [bulbasaur, charmander, squirtle];
+const cardTypes = ["Grass Type Pokémon", "Fire Type Pokémon", "Water Type Pokémon"];
 
 //Add an event listener to the start button
 document.getElementById("start-button").addEventListener("click", function () {
@@ -31,7 +32,7 @@ function startGame() {
 
     if (playerName === "") {
         //Default name if the input is empty
-        playerName = "You"; 
+        playerName = "You";
         document.querySelector(".name-input").style.display = "none";
     } else {
         //Hide the name input div if a name is entered
@@ -45,16 +46,17 @@ function startGame() {
     document.getElementById("player-choice").src = empty;
     document.getElementById("opponent-choice").src = empty;
 
-    //Creat Cards options
+    //Create Cards options
     for (let i = 0; i < 3; i++) {
         let card = document.createElement("img");
         card.id = cards[i];
         card.src = cards[i];
+        card.alt = cardTypes[i];
         card.addEventListener("click", selectCard);
         document.getElementById("cards").append(card);
     }
 
-    //Creat number of rounds
+    //Create number of rounds
     for (let j = 0; j < 5; j++) {
 
         let round = document.createElement("img");
@@ -62,64 +64,12 @@ function startGame() {
         round.src = pokeball;
         document.getElementById("pokeballs").append(round);
     }
-};
+}
 
-function selectCard() {
-    player = this.id;
-    document.getElementById("player-choice").src = player;
 
-    opponent = cards[Math.floor(Math.random() * 3)];
-    document.getElementById("opponent-choice").src = opponent;
-
-    //Check scores
-    if (player == opponent) {
-
-    }
-    else {
-        //Grass type beat water type
-        if (player == bulbasaur) {
-            if (opponent == squirtle) {
-                playerScore += 1;
-            }
-            //Fire beat Grass
-            else if (opponent == charmander) {
-                opponentScore += 1;
-            }
-        }
-        //Fire beat grass
-        else if (player == charmander) {
-            if (opponent == bulbasaur) {
-                playerScore += 1;
-            }
-            //Water beat Fire
-            else if (opponent == squirtle) {
-                opponentScore += 1;
-            }
-        }
-        //Water beat Fire
-        else if (player == squirtle) {
-            if (opponent == charmander) {
-                playerScore += 1;
-            }
-            //Grass beat Water
-        else if (opponent == bulbasaur) {
-                opponentScore += 1;
-            }
-        }
-    }
-
-    //Add Score
-    document.getElementById("player-score").innerText = playerScore;
-    document.getElementById("opponent-score").innerText = opponentScore;
-
-    //Remove a round element
-    let rounds = document.querySelectorAll("#pokeballs img");
-    if (rounds.length > 0) {
-        document.getElementById("pokeballs").removeChild(rounds[0]);
-    }
-
+function checkWinner(rounds) {
     //Check if there are no more rounds left
-    if (rounds.length === 0) {
+    if (rounds.length - 1 === 0) {
         // Restart the game
         let resultMessage = "Looks like it's a tie, Trainer!";
         if (playerScore > opponentScore) {
@@ -127,30 +77,87 @@ function selectCard() {
         } else if (playerScore < opponentScore) {
             resultMessage = "Don't give up, Trainer! Your next battle awaits!";
         }
+        setTimeout(function () {
+            alert(resultMessage);
+            gameEnded = true;
+            // Reset the scores to 0
+            playerScore = 0;
+            opponentScore = 0;
+            document.getElementById("player-score").innerText = playerScore;
+            document.getElementById("opponent-score").innerText = opponentScore;
 
-        alert(resultMessage);
+            // Reset player and opponent choices to the placeholder image
+            document.getElementById("player-choice").src = empty;
+            document.getElementById("opponent-choice").src = empty;
 
-        gameEnded = true;
+            // Re-add round elements
+            for (let j = 0; j < 5; j++) {
+                let round = document.createElement("img");
+                round.id = "pokeball";
+                round.src = pokeball;
+                document.getElementById("pokeballs").append(round);
+            }
+        }, 800);
+        return true;
+    }
+    return false;
 
-        // Reset the scores to 0
-        playerScore = 0;
-        opponentScore = 0;
+}
+
+function selectCard() {
+    player = this.id;
+    //Add image to arena when the card is selected
+    document.getElementById("player-choice").src = player;
+    opponent = cards[Math.floor(Math.random() * 3)];
+    document.getElementById("opponent-choice").src = opponent;
+    //Add alt elements
+    document.getElementById("player-choice").alt = cardTypes[cards.indexOf(player)];
+    document.getElementById("opponent-choice").src = opponent;
+    document.getElementById("opponent-choice").alt = cardTypes[cards.indexOf(opponent)];
+    let rounds = document.querySelectorAll("#pokeballs img");
+    if (checkWinner(rounds) == false) {
+        //Check scores
+        if (player != opponent) {
+            //Grass type beat water type
+            if (player == bulbasaur) {
+                if (opponent == squirtle) {
+                    playerScore += 1;
+                }
+                //Fire beat Grass
+                else if (opponent == charmander) {
+                    opponentScore += 1;
+                }
+            }
+            //Fire beat grass
+            else if (player == charmander) {
+                if (opponent == bulbasaur) {
+                    playerScore += 1;
+                }
+                //Water beat Fire
+                else if (opponent == squirtle) {
+                    opponentScore += 1;
+                }
+            }
+            //Water beat Fire
+            else if (player == squirtle) {
+                if (opponent == charmander) {
+                    playerScore += 1;
+                }
+                //Grass beat Water
+                else if (opponent == bulbasaur) {
+                    opponentScore += 1;
+                }
+            }
+        }
+
+        //Add Score
         document.getElementById("player-score").innerText = playerScore;
         document.getElementById("opponent-score").innerText = opponentScore;
-
-        // Reset player and opponent choices to the placeholder image
-        document.getElementById("player-choice").src = empty;
-        document.getElementById("opponent-choice").src = empty;
-
-        // Re-add round elements
-        for (let j = 0; j < 5; j++) {
-            let round = document.createElement("img");
-            round.id = "pokeball";
-            round.src = pokeball;
-            document.getElementById("pokeballs").append(round);
-        }
     }
-
+    //Remove a round element
+    if (rounds.length > 0) {
+        document.getElementById("pokeballs").removeChild(rounds[0]);
+    }
 }
 
 // Add a click event listener to the "info-button" element
